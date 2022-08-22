2c89350c818ea1e24e965b262882e2fd50a01d37
AdrianaHavrylyuk <81040618+AdrianaHavrylyuk@users.noreply.github.com>
2022-08-23 02:59:09 +0300

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app2.Models
+{
+    public class Product
+    {
+        public string name { get; set; }
+        public double price { get; set; }
+    }
+}
---
+++ TasksController.cs
@@ -0,0 +1,62 @@
+﻿using app2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace app2.Controllers
+{
+    public class TasksController : Controller
+    {
+        public TasksController()
+        { }
+        IList<Product> prodList = new List<Product>() { 
+             new Product { name = "milk", price = 11},
+             new Product { name = "bread", price = 10 },
+             new Product { name = "cheese", price =140},
+             new Product { name = "sausage", price =110},
+             new Product { name = "potato", price = 7 },
+             new Product { name = "banana", price = 23 },
+             new Product { name = "tomato", price = 25 },
+             new Product { name = "candy", price = 75 }
+        };
+        public IActionResult Greetings()
+        {
+            DateTime date = DateTime.Today;
+            ViewBag.day = date.DayOfWeek;
+            DateTime time = DateTime.Now;
+            if ( ( time.Hour >= 4 ) & (time.Hour <= 10 )) { ViewBag.greet = "Good Morning"; }
+            else { ViewBag.greet = "Good Evening"; }
+            return View();
+        }
+        public IActionResult ProductInfo()
+        {
+            ViewBag.prod =  prodList;
+            return View();
+
+        }
+        public IActionResult ShoppingCart()
+        {
+            return View();
+
+        }
+        public IActionResult ShoppingList()
+        {
+            return View();
+
+        }
+        public IActionResult Supermarkets()
+        {
+            var stringArray = new string[] { "Wellmart", "Silpo", "ATB" , "Furshet", "Metro"};
+            ViewBag.Collection = stringArray;
+            ViewBag.count = stringArray.Length;
+
+            return View();
+
+        }
+    }
+}
